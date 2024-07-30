70b6ee0f4402d65c0c3906d2c60b424743d9533a
Katherine Álvarez Bustos <katherine.albu@gmail.com>
2024-07-29 22:22:54 -0600

    Menú mobile agregado y toggle de menús mejorado

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const emailMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 // Si hace click en el menu del email, hacer toggle del desktop menu
 emailMenu.addEventListener('click', toggleMenu); 
+burgerMenu.addEventListener('click', toggleMobileMenu);
+
 
 function toggleMenu() {
    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
