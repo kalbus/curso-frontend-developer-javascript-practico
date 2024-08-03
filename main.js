bf8f1f02c6d5ed6cbb0f92de8bf498e94042b42a
Katherine Álvarez Bustos <katherine.albu@gmail.com>
2024-08-03 00:29:08 -0600

    Integración con My Order Menu

--- main.js
+++ main.js
@@ -2,16 +2,46 @@ const emailMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const carMenu = document.querySelector('.navbar-shopping-cart');
+const asideMyOrder = document.querySelector('.product-detail');
 
 // Si hace click en el menu del email, hacer toggle del desktop menu
 emailMenu.addEventListener('click', toggleMenu); 
 burgerMenu.addEventListener('click', toggleMobileMenu);
+carMenu.addEventListener('click', toggleasideMyOrder);
 
 
 function toggleMenu() {
+   isasideMyOrderActive = !asideMyOrder.classList.contains('inactive');
+
+   if (isasideMyOrderActive) {
+    asideMyOrder.classList.add('inactive');
+   }
+
    desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    isasideMyOrderActive = !asideMyOrder.classList.contains('inactive');
+
+    if (isasideMyOrderActive) {
+        asideMyOrder.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleasideMyOrder() {
+    isDesktopMenuActive = !desktopMenu.classList.contains('inactive');
+    isMobileMenuActive = !mobileMenu.classList.contains('inactive');
+
+    if (isDesktopMenuActive) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    if (isMobileMenuActive) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    asideMyOrder.classList.toggle('inactive');
 }
